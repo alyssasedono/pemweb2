c0af1adfb2c0dfda324b19bf30711271c9515bc5
alyssasedono <alyssasedono14@gmail.com>
2024-02-21 20:35:45 +0700

    first commit

---
+++ praktikum1/array_asosiatif.php
@@ -0,0 +1,9 @@
+<?php
+$mhs1 = [
+    'nama' => 'Alyssa Sedono', 
+    'nim' => '0110123044', 
+    'jurusan' => 'Sistem Informasi'
+];
+
+// memanggil array asosiatif
+echo 'Nama: ' . $mhs1['nama'];
---
+++ praktikum1/variable.php
@@ -0,0 +1,8 @@
+<?php
+// membuat variable
+$mahasiswa1 = ["Alyssa Sedono", "Sistem Informasi", 0110123044 , [3.9]];
+
+// panggil variable
+echo 'Nama: ' . $mahasiswa1[0] . "<br>";
+echo "Jurusan: $mahasiswa1[1] <br>" ;
+echo "IPS Semester 1: " . $mahasiswa1[3] [0];
